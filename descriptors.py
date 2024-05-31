cc9306f85685917b3a7abd974ccdfb28b6007a73
MatejBevec <matejbevec98@gmail.com>
2024-05-31 10:58:18 +0200

    Feature selection for descriptors

--- descriptors.py
+++ descriptors.py
@@ -1,5 +1,11 @@
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.feature_selection import RFE, RFECV, SelectKBest
 
 from propy import PyPro, CTD, PseudoAAC
 import pfeature
@@ -7,14 +13,15 @@ import pfeature
 
 
 
-def load_data(path, shuf=True):
+def load_data(path, shuf=False):
     """Load protein sequence dataset."""
 
     df = pd.read_csv(path)
     df["input"] = df["input"].apply(lambda x: x.strip().replace(" ", ""))
+    df = df[df["membrane"] != "U"] # remove uknown targets
 
     if shuf:
-        df = df.iloc[np.random.permutation(len(df)), :]
+        df = df.iloc[np.random.RandomState(42).permutation(len(df)), :]
 
     sequences = np.array(df["input"])
     classes, y = np.unique(df["membrane"], return_inverse=True)
@@ -82,18 +89,67 @@ def extract_descriptors(sequences):
     return features, descriptor_names, feature_names
 
 
+def train_test_split(X, y, ratio=0.7):
+    split = int(len(features)*0.7)
+    X_train = X[:split]; X_test = X[split:]
+    y_train = y[:split]; y_test = y[split:]
 
+    return X_train, y_train, X_test, y_test
+
+def shuffle_data(X, y):
+    print(X.shape, y.shape)
+    shuf = np.random.RandomState(42).permutation(len(X))
+
+    return X[shuf], y[shuf]
 
 
 if __name__ == "__main__":
 
-    sequences, y, classes = load_data("data/deeploc_per_protein_train.csv")
+    sequences, y, classes = load_data("data/deeploc_per_protein_test.csv")
 
-    features, desc_names, feature_names = extract_descriptors(sequences)
+    print(len(sequences))
 
-    np.save("descriptors_train.npy", [features, desc_names, feature_names], allow_pickle=True)
+    #features, desc_names, feature_names = extract_descriptors(sequences)
 
-    features, desc_names, feature_names = np.load("descriptors_train.npy", allow_pickle=True)
+    #np.save("descriptors_test.npy", [features, desc_names, feature_names], allow_pickle=True)
+
+    # features, desc_names, feature_names = np.load("descriptors_train.npy", allow_pickle=True)
+
+
+    # DATA
+
+    sequences_train, y_train, classes = load_data("data/deeploc_per_protein_train.csv")
+    X_train, _, _ = np.load("descriptors_test.npy", allow_pickle=True)
+    sequences_test, y_test, classes = load_data("data/deeploc_per_protein_test.csv")
+    X_test, _, _ = np.load("descriptors_test.npy", allow_pickle=True)
+
+    X_train, y_train = shuffle_data(X_train, y_train)
+    X_test, y_test = shuffle_data(X_test, y_test)
+
+    # CLASSIFIERS
+
+    #clf = RandomForestClassifier()
+    clf = SVC(C=2)
+    #clf = MLPClassifier()
+
+    # DIM REDUCTION
+
+    proj_model = PCA(n_components=1000)
+    #proj_model = RFE(estimator=clf, n_features_to_select=1000, verbose=True)
+    #proj_model = SelectKBest(k=1000)
+    #clf = clf.fit(features, y)
+    X_train = proj_model.fit_transform(X_train, y=y_train)
+    X_test = proj_model.transform(X_test)
+
+
+    clf.fit(X_train, y_train)
+
+
+    pred = clf.predict(X_test)
+    #pred = np.random.choice([0,1,2], size=(len(y),))
+    ca = accuracy_score(y_test, pred)
+
+    print(ca)
 
 
 
